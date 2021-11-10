d75bd62d73e523cce1f9de5c84e70b647ddf838b
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-10 18:21:55 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<=n;i++)
           dp[i][0]=1;
         for(int i=1;i<=r;i++)
-          dp[0][i]=1;
+          dp[0][i]=0;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=r;j++)
