b3de01c56bf9dd41542b116725375b8613e538ce
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-22 12:58:51 +0530

    Update Knapsack with Duplicate Items.cpp

--- Knapsack with Duplicate Items.cpp
+++ Knapsack with Duplicate Items.cpp
@@ -6,9 +6,9 @@ public:
     {
         vector<vector<int>> dp(n+1,vector<int> (w+1));
        
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<=n;i++)
         {
-            for(int j=1;j<=w;j++)
+            for(int j=0;j<=w;j++)
             {
                 if(i==0 or j==0)
                   dp[i][j]=0;
