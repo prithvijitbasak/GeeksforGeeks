2e315437c39ab289b4b66b5ee53261f1180a866d
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-26 20:00:01 +0530

    Create Longest Repeating Subsequence.cpp

---
+++ Longest Repeating Subsequence.cpp
@@ -0,0 +1,22 @@
+//Editorial: https://youtu.be/hbTaCmQGqLg
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    int n=str.size();
+		    vector<vector<int>> dp(n+1,vector<int> (n+1));
+		    for(int i=0;i<=n;i++)
+		    {
+		        for(int j=0;j<=n;j++)
+		        {
+		            if(i==0 or j==0)
+		              dp[i][j]=0;
+		          else if(str[i-1]==str[j-1] and i!=j)
+		            dp[i][j]=1+dp[i-1][j-1];
+		           else 
+		             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		        }
+		    }
+		    return dp[n][n];
+		}
+
+};
