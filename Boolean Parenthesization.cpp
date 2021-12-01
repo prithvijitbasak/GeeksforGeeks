bd73bfb9b533257cdb4950ebae2ff333cb9f6129
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-12-01 14:33:54 +0530

    Create Boolean Parenthesization.cpp

---
+++ Boolean Parenthesization.cpp
@@ -0,0 +1,56 @@
+//Editorial: https://youtu.be/bzXM1Zond9U
+class Solution{
+public:
+    int dp[201][201][2];
+    int countWays(int n, string s)
+    {
+        memset(dp,-1,sizeof(dp));
+        mem(s,0,n-1,true);
+    }
+    int mem(string &s,int i,int j,bool istrue)
+    {
+        if(i>j)
+          return 0;
+        
+        if(dp[i][j][istrue]!=-1)
+          return dp[i][j][istrue];
+        if(i==j)
+        {
+            if(istrue==true)
+              return s[i]=='T';
+            else
+              return s[i]=='F';
+            // return dp[i][j][istrue];
+        }
+        int ans=0;
+        for(int k=i+1;k<j;k+=2)
+        {
+            int lt=mem(s,i,k-1,true);
+            int rt=mem(s,k+1,j,true);
+            int lf=mem(s,i,k-1,false);
+            int rf=mem(s,k+1,j,false);
+            if(s[k]=='|')
+            {
+                if(istrue==true)
+                  ans+=lt*rt+lt*rf+lf*rt;
+                else
+                  ans+=lf*rf;
+            }
+            else if(s[k]=='&')
+            {
+                if(istrue==true)
+                  ans+=rt*lt;
+                else
+                  ans+=rf*lf+rt*lf+lt*rf;
+            }
+            else if(s[k]=='^')
+            {
+                if(istrue==true)
+                  ans+=lt*rf+lf*rt;
+                else
+                  ans+=lt*rt+lf*rf;
+            }
+        }
+        return dp[i][j][istrue]=ans%1003;
+    }
+};
